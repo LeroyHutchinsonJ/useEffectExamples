59c751bc0f2f636835e61a7b5119ada1324a998a
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-10-13 13:30:15 -0400

    Created the log effect component
    Created the log effect component which will spear head my introduction of the useEffect hook

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div className="App">Blah Blah</div>;
+  return <div className="App">Blah Blah Blah</div>;
 }
---
+++ src/logEffect.js
@@ -0,0 +1,5 @@
+import React, { useState, useReducer } from "react";
+
+var LogEffect = () => {
+  return <div>Blah Blah Blah</div>;
+};
