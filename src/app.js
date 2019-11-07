09877d426177f93f9eae320a58e5cf4b20e403f7
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-11-07 09:12:14 -0500

    Got the information
    Added the json for the reddit information, and posts it as a list

--- src/app.js
+++ src/app.js
@@ -4,5 +4,13 @@ import LogEffect from "./logEffect";
 import Reddit from "./reddit";
 
 export default function App() {
+  const [inputValue, setInputValue] = useState("reactjs");
+
+  const [subReddit, setSubReddit] = useState(inputValue);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
   return <Reddit className="App" />;
 }
