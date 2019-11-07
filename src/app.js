7aecfce7d7b0c264febd29934ef165433b4002a7
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-11-07 09:27:12 -0500

    Created an input field
    The input field allows the user the to choose what subreddit they want to display

--- src/app.js
+++ src/app.js
@@ -10,7 +10,19 @@ export default function App() {
 
   const handleSubmit = e => {
     e.preventDefault();
-  };
 
-  return <Reddit className="App" />;
+    setSubReddit(inputValue);
+  };
+  //Check
+  return (
+    <>
+      <form onSumbit={handleSubmit}>
+        <input
+          value={inputValue}
+          onChange={e => setInputValue(e.target.value)}
+        />
+      </form>
+      <Reddit subReddit={inputValue} />
+    </>
+  );
 }
