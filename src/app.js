d6a374c872fdebf85e3835fae26aafa9ef21896e
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-11-07 09:21:52 -0500

    InputField to change subreddit
    I created an input field that will change the sub reddit depending on what the user types

--- src/app.js
+++ src/app.js
@@ -4,5 +4,25 @@ import LogEffect from "./logEffect";
 import Reddit from "./reddit";
 
 export default function App() {
-  return <Reddit className="App" />;
+  const [inputValue, setInputValue] = useState("reactjs");
+
+  const [subReddit, setSubReddit] = useState(inputValue);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    setSubReddit(inputValue);
+  };
+
+  return (
+    <>
+      <form onSumbit={handleSubmit}>
+        <input
+          value={inputValue}
+          onChange={e => setInputValue(e.target.value)}
+        />
+      </form>
+      <Reddit subReddit={inputValue} />
+    </>
+  );
 }
--- src/reddit.js
+++ src/reddit.js
@@ -1,8 +1,23 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-var Reddit = () => {
-  return <div>Blah Blah</div>;
+var Reddit = ({ subReddit = "reactjs" }) => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    //Fetch the data
+    fetch(`https://www.reddit.com/r/${subReddit}.json`)
+      .then(res => res.json())
+      .then(json => setPosts(json.data.children.map(c => c.data)));
+  }, [subReddit, setPosts]);
+
+  return (
+    <ul>
+      {posts.map(post => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default Reddit;
